03be1bbe15a308489167ac95588de222b2a220d4
Marcos Victor Sousa do Vale <111076997+marcosvaledev@users.noreply.github.com>
2023-11-21 13:19:13 -0300

    feat: aula-03

---
+++ aula-03.js
@@ -0,0 +1,10 @@
+const idade = 1;
+const nome = 'ju1lio';
+let podeEntrar = false;
+if(idade >= 18 || nome === 'julio'){
+   podeEntrar = true;
+   console.log('pode entrar',podeEntrar);
+}else{
+    console.log('nao entra!!')
+}
+
