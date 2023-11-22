ed1b773792270078e7a95a48fdefb76b15b3a3a7
Marcos Victor Sousa do Vale <111076997+marcosvaledev@users.noreply.github.com>
2023-11-22 13:05:05 -0300

    feat: aula-04 for structure repetion

---
+++ aula-04.js
@@ -0,0 +1,17 @@
+/*const quantidade = 10;
+let soma = 1;
+
+for(let i = 0; i < quantidade; i++){
+    console.log(i)
+    soma = soma + i
+}
+console.log(soma);*/
+
+//tabuada simples
+let valTabuada = 1;
+
+for(let i = 0; i <= 20; i++){
+    console.log("a tabuada é de " + valTabuada + "x" + i + "=" + (valTabuada * i)) 
+}
+
+
