0d51a5598a454745a081c93dd81b43a54f41c1d0
Marcos Victor Sousa do Vale <111076997+marcosvaledev@users.noreply.github.com>
2023-11-22 13:48:34 -0300

    feat: function basic

---
+++ aula-09.js
@@ -0,0 +1,11 @@
+function soma(item1, item2) {
+    return item1 + item2;
+}
+
+console.log('SOMA ', soma(5, 6));
+
+
+function diminuir(item1, item2) {
+    return item1 - item2;
+}
+console.log(diminuir(5, 4))
